9b19d1c5b7aca01bf53e384deae87413de6f31d7
PatrickBett <patrickbett018@gmail.com>
2024-01-15 18:27:41 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,16 @@
 import requests
 import json
 
+
 class GetRequester:
 
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        parsed_data = json.loads(self.get_response_body())
+        return parsed_data
